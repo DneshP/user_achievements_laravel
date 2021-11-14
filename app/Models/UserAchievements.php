13ef5877b9d425d9c99de325354c370e55bf61c8
its_d <dineshparadesi94@gmail.com>
2021-11-14 10:29:29 +1000

    Added User Achivements Migration and Model

---
+++ app/Models/UserAchievements.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserAchievements extends Model
+{
+    use HasFactory;
+    
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'achievement_id',
+        'user_id',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * The Achievements unlocked by the user.
+     */
+    public function unlockedAchievements()
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
