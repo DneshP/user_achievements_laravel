68943cb0953b761de9db93a5f0c12ae14fbb5e94
its_d <dineshparadesi94@gmail.com>
2021-11-14 11:22:57 +1000

    Added routest to trigger events

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AchievementsController;
+use App\Models\User;
+use App\Events\LessonWatched;
+use App\Events\CommentWritten;
+use App\Models\Lesson;
+use App\Models\Comment;
 
 Route::get('/users/{user}/achievements', [AchievementsController::class, 'index']);
+Route::get('/lessonWatched', function() {
+    $user = User::find(1);
+    event(new LessonWatched(new Lesson, $user));
+});
+Route::get('/commentWritten', function() {
+    $comment = Comment::find(1);
+    event(new CommentWritten($comment));
+});
