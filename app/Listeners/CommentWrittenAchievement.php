85cdfc932516a59accd0bad0ce469bf06a4894dc
its_d <dineshparadesi94@gmail.com>
2021-11-14 10:46:04 +1000

    Added Listener CommentWrittenAchivement

---
+++ app/Listeners/CommentWrittenAchievement.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\CommentWritten;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
+use App\Models\UserAchievements;
+use App\Models\AchievementsList;
+
+class CommentWrittenAchievement
+{
+    use SerializesModels;
+
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  CommentWritten  $event
+     * @return void
+     */
+    public function handle(CommentWritten $event)
+    {
+        $collection = $event->comment->user()->get();
+        $user;
+        foreach ($collection as $value) {
+            $user = $value;
+        }
+        $commentsWrittenByUser = count($user->comments()->get());
+        $commentWrittenAchievements = AchievementsList::where('type', 'Comment')->get();
+        $achivementIds = [];
+        foreach ($commentWrittenAchievements as  $value) {
+            $achivementIds[] = $value->id;
+        }
+        $getuserAchievements = UserAchievements::select('achievement_id')
+                                            ->where('user_id', $user->id)
+                                            ->whereIn('achievement_id', $achivementIds)
+                                            ->get();
+        $unlockedAchievements = [];
+        foreach ($getuserAchievements as $value) {
+            $unlockedAchievements[] = $value->achievement_id;
+        }
+        foreach ($commentWrittenAchievements as $achievement) {
+            if ($achievement->count === $commentsWrittenByUser && !in_array($achievement->id, $unlockedAchievements)) {
+                // Unlock Achievement yay
+                $inserted = UserAchievements::create([
+                    'achievement_id' => $achievement->id,
+                    'user_id' => $user->id
+                ]);
+                $unlockedAchievements[] = $inserted->achievement_id;
+            }
+        }
+
+    }
+}
