014faa6ec69358d5c0c965c4f7582f2f78626ea6
its_d <dineshparadesi94@gmail.com>
2021-11-14 11:20:43 +1000

    Registered Events

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -4,6 +4,12 @@ namespace App\Providers;
 
 use App\Events\LessonWatched;
 use App\Events\CommentWritten;
+use App\Events\AchievementUnlocked;
+use App\Events\BadgeUnlocked;
+use App\Listeners\LessonWatchedAchievement;
+use App\Listeners\CommentWrittenAchievement;
+use App\Listeners\AchievementUnlockedNotification;
+use App\Listeners\BadgeUnlockedNotification;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
@@ -16,11 +22,17 @@ class EventServiceProvider extends ServiceProvider
      */
     protected $listen = [
         CommentWritten::class => [
-            //
+            CommentWrittenAchievement::class
         ],
         LessonWatched::class => [
-            //
+            LessonWatchedAchievement::class
         ],
+        AchievementUnlocked::class => [
+            AchievementUnlockedNotification::class
+        ],
+        BadgeUnlocked::class => [
+            BadgeUnlockedNotification::class
+        ]
     ];
 
     /**
