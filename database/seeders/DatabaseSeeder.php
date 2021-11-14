52facbe0631e354c227a21e3069a7b389af033dd
its_d <dineshparadesi94@gmail.com>
2021-11-14 11:30:19 +1000

    Added Database Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,13 @@
 namespace Database\Seeders;
 
 use App\Models\Lesson;
+use App\Models\User;
+use App\Models\Comment;
+use App\Models\UserAchievements;
 use Illuminate\Database\Seeder;
+use Database\Seeders\AchievementListSeeder;
+use Database\Seeders\UserLessonSeeder;
+use Database\Seeders\BadgeListSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,8 +20,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $user = User::factory()
+        ->count(3)
+        ->create();
+
         $lessons = Lesson::factory()
-            ->count(20)
-            ->create();
+        ->count(10)
+        ->create();
+
+        $comments = Comment::factory()
+        ->count(1)
+        ->create();
+
+        $this->call([
+            AchievementListSeeder::class,
+            UserLessonSeeder::class,
+            BadgeListSeeder::class
+        ]);
     }
 }
