475609be7d1c8a6d0646ae047bb247a383c27780
its_d <dineshparadesi94@gmail.com>
2021-11-19 22:43:25 +1000

    added improvements

--- app/Listeners/LessonWatchedAchievement.php
+++ app/Listeners/LessonWatchedAchievement.php
@@ -43,11 +43,12 @@ class LessonWatchedAchievement
          * doing a check here.
          * Alternatively we could trigger a event when a user registers for a course in this scenario
          */
-        $currentAchievements = count($user->unlockedAchievements()->where('user_id', $user->id)->get());
-        $userBadges = $user->userBadges()->where('user_id', '=', $user->id)->get();
-        $BadgeList = new BadgeList();
-        foreach ($BadgeList->badgeList()->get() as $badge) {
-            if (count($userBadges) === 0 && $badge->count === $currentAchievements ) {
+        $currentAchievements = count($user->achievements);
+        $userBadges = $user->badges()->get()->toArray();
+        $userBadgeIds = array_map(fn($value) => $value['badge_id'], $userBadges);
+        $badgeList = BadgeList::orderBy('order', 'asc')->get();
+        foreach ($badgeList as $badge) {
+            if (!in_array($badge->id, $userBadgeIds) && $badge->count === $currentAchievements) {
             $inserted = UserBadge::create([
                     'user_id' => $user->id,
                     'badge_id' => $badge->id
@@ -56,32 +57,26 @@ class LessonWatchedAchievement
                 break;
             }
         }
+
         $lesson = $event->lesson;
-        $lessonsWatchedByUser = count($user->watched()->get());
-        $lessonWatchedAchievements = AchievementsList::where('type', 'Lesson')->get();
+        $lessonsWatchedByUser = count($user->watched);
+        $lessonWatchedAchievements = AchievementsList::select('id', 'name', 'count')->where('type', 'Lesson')->get();
         $achivementIds = [];
         foreach ($lessonWatchedAchievements as  $value) {
             $achivementIds[] = $value->id;
         }
-
-        $getuserAchievements = UserAchievements::select('achievement_id')
+        $unlockedAchievements = UserAchievements::select('achievement_id')
                                             ->where('user_id', $user->id)
                                             ->whereIn('achievement_id', $achivementIds)
-                                            ->get();
-
-        $unlockedAchievements = [];
-        foreach ($getuserAchievements as $value) {
-            $unlockedAchievements[] = $value->achievement_id;
-        }
+                                            ->get()->toArray();
+        $unlockedAchievementIds = array_map(fn($value) => $value['achievement_id'], $unlockedAchievements);
         foreach ($lessonWatchedAchievements as $key => $achievement) {
-            
-            if ($achievement->count === $lessonsWatchedByUser && !in_array($achievement->id, $unlockedAchievements)) {
+            if ($achievement->count === $lessonsWatchedByUser && !in_array($achievement->id, $unlockedAchievementIds)) {
                 // Unlock Achievement yay
                 $inserted = UserAchievements::create([
                     'achievement_id' => $achievement->id,
                     'user_id' => $user->id
                 ]);
-                $unlockedAchievements[] = $inserted->achievement_id;
                 event(new AchievementUnlocked($achievement->name, $user));
             }
         }
